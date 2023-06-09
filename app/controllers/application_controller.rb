5b562ea90435e227a26fa76ac6ea373620eccc04
Marucha Johnstone <marucha.webdev@yahoo.com>
2023-06-09 23:57:48 +0300

    feat: Index route with a message to test comm with frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    def index
+        render json: { message: "Sample application with Role based Auth & ActionMailer functionality"}
+    end
+
 end
